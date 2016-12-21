6004979247658195c30d1d382b044e2f971121e6
James <jamezbrown100@gmail.com>
2016-12-21 13:11:27 -0600

    Add id to cell and return in in cell for row

--- Clock/TimerVC.swift
+++ Clock/TimerVC.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class TimerVC: UIViewController {
-
+class TimerVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TimerCell") {
+            return cell
+        }
+        
+        return UITableViewCell()
     }
 
 }
