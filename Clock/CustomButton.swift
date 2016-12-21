e07eb956d8059176ce25d67837ace19596b12458
James <jamezbrown100@gmail.com>
2016-12-21 12:56:06 -0600

    Add shadow to custom button class and apply it to add button

--- Clock/CustomButton.swift
+++ Clock/CustomButton.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 @IBDesignable class CustomButton: UIButton {
+    @IBInspectable var shadowColor: UIColor = UIColor.black {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 1 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = CGSize.zero {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
             layer.cornerRadius = cornerRadius
