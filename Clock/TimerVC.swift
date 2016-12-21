2162b645fa999ec8b6a4211b74d96ae93754ef40
James <jamezbrown100@gmail.com>
2016-12-21 13:06:38 -0600

    Add timer vc class

---
+++ Clock/TimerVC.swift
@@ -0,0 +1,18 @@
+//
+//  TimerVC.swift
+//  Clock
+//
+//  Created by James Brown on 12/21/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class TimerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
