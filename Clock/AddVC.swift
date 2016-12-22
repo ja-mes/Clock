52fa0aae76da873ff47d448f6e7e561254ed5e1e
James <jamezbrown100@gmail.com>
2016-12-21 18:38:06 -0600

    Create addvc class

---
+++ Clock/AddVC.swift
@@ -0,0 +1,17 @@
+//
+//  AddVC.swift
+//  Clock
+//
+//  Created by James Brown on 12/21/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class AddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
