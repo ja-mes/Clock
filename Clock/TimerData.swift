a1c038a2f8d4d1cd9cc95265ea4448d05b2498c9
James <jamezbrown100@gmail.com>
2016-12-22 13:27:12 -0600

    Get seconds rounded to the nearest .01 seconds

--- Clock/TimerData.swift
+++ Clock/TimerData.swift
@@ -73,11 +73,11 @@ class TimerData {
     }
     
     func tick(with timerEntity: TimerEntity, to label: UILabel) {
-        let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { (timer) in
             if let startDate = timerEntity.startDate {
                 let seconds = Date().timeIntervalSince(startDate as Date)
                 
-                label.text = "\(seconds)"
+                label.text = "\((Double(round(100*seconds)/100)))"
             }
         }
     }
