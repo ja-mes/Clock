f134352fae61188c9d33c9b492e038db8d5cfa87
James <jamezbrown100@gmail.com>
2016-12-21 20:45:42 -0600

    Create timer data class

---
+++ Clock/Timer.swift
@@ -0,0 +1,25 @@
+//
+//  Timer.swift
+//  Clock
+//
+//  Created by James Brown on 12/21/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import Foundation
+
+class TimerData {
+    private var _name: String
+    
+    var name: String {
+        get {
+            return _name
+        } set {
+            _name = newValue
+        }
+    }
+    
+    init(name: String) {
+        _name = name
+    }
+}
