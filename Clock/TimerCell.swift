8d02a0737e8ffa8d289b7e3ff25f1fb2cfafaa50
James <jamezbrown100@gmail.com>
2016-12-21 20:41:31 -0600

    Create timer cell class

---
+++ Clock/TimerCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimerCell.swift
+//  Clock
+//
+//  Created by James Brown on 12/21/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class TimerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
